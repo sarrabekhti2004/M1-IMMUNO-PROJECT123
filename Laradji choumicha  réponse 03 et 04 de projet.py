a8c2ba57d857dd7f8d280844d823d648a4f26c4c
Sarra Bekhti <sarrabekhti98@gmail.com>
2025-12-13 09:54:42 +0100

    Fix print statement for average GC calculation
    Added a newline in the average GC calculation print statement.

--- Laradji choumicha  réponse 03 et 04 de projet.py
+++ Laradji choumicha  réponse 03 et 04 de projet.py
@@ -14,7 +14,7 @@ filtered_df = df[df["Longueur"]>10]
 print(filtered_df)
 
 #4)Calculer la moyenne du pourcentage de GC
-print("********* Calcul de la moyenne **********")
+print("********* Calcul de la moyenne **********","/n""n")
 # Calculer la moyenne du pourcentage de GC
 df = pd.DataFrame(data)
 average_gc = df["Pourcentage GC"].mean()
