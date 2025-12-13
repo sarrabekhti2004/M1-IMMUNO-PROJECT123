78d7438e1d6fd3124314ab36fda6fed00df53288
kechenourelhouda <houdaanas2014@gmail.com>
2025-12-13 14:36:42 +0100

    Update les réponses complétés .py

--- les réponses complétés .py
+++ les réponses complétés .py
@@ -104,5 +104,5 @@ print("\n**************** TABLEAU FINAL AVEC TOUTES LES VARIATIONS *************
 print(df)
 df.to_csv("tableau_séquences_final.csv", index=False)
 #Charger un fichier CSV dans un DataFrame 
-df_loaded = pd.read_csv("tableau_séquences.csv")
+df_loaded = pd.read_csv("tableau_séquences_final.csv")
 print(df_loaded)
