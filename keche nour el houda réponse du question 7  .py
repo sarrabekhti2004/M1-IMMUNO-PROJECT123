e7fe20559af0166eb91462f5bbf473bd2a029388
kechenourelhouda <houdaanas2014@gmail.com>
2025-12-13 11:24:03 +0100

    Update keche nour el houda réponse du question 7  .py

--- keche nour el houda réponse du question 7  .py
+++ keche nour el houda réponse du question 7  .py
@@ -5,6 +5,11 @@ data = {
     "Longueur":[12, 12, 12, 10, 11, 10, 10],
     "Pourcentage GC": [50, 66.67, 58.33, 40, 45.45, 60, 50]
     }
+#Moyenne du Pourcentage de GC
+nom_de_la_colonne = "Pourcentage GC"
+moyenne_gc = df[nom_de_la_colonne].mean()
+print(f"La moyenne de {nom_de_la_colonne} est : {moyenne_gc:.2f}")
+
 #Création d'un DataFrame (tableau pandas)  
 df = pd.DataFrame(data)
 print("**************** Creation et affichage ****************","\n""\n")
