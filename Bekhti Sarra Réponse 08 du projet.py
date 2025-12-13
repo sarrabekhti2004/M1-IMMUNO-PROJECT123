4efddb336799e96678dee28e7c92fd7fb27e2e0b
kechenourelhouda <houdaanas2014@gmail.com>
2025-12-13 11:41:57 +0100

    Update Bekhti Sarra Réponse 08 du projet.py

--- Bekhti Sarra Réponse 08 du projet.py
+++ Bekhti Sarra Réponse 08 du projet.py
@@ -1,6 +1,10 @@
-#8) Sauvegarder le tableau final dans un fichier CSV.
-df.to_csv("tableau_séquences.csv", index=False)
+#8)Sauvegarder le tableau final dans un fichier CSV.
+df["Nombre de G"] = df["Séquence"].apply(lambda x: x.count("G"))
+df["Catégorie Longueur"] = df["Longueur"].apply(lambda x: "Longue" if x >= 12 else "Courte/Moyenne")
+print("\n**************** TABLEAU FINAL AVEC TOUTES LES VARIATIONS ****************","\n")
+print(df)
 
+df.to_csv("tableau_séquences_final.csv", index=False)
 #Charger un fichier CSV dans un DataFrame 
 df_loaded = pd.read_csv("tableau_séquences.csv")
 print(df_loaded)
